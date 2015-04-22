237e071fe092109111f34dcdc78595cf6747f9d9
railgan <sclol@hotmail.com>
2015-04-22 08:56:39 +0200

    added Mangel_Models

---
+++ 05_Eclipseprojekt/ch.hsluw.mangelmanager.model/src/ch/hsluw/mangelmanager/model/Mangel.java
@@ -0,0 +1,210 @@
+/*
+ * ZWECK: Mangelmanager
+ * MODUL: Softwarekompomenten, HSLU-Wirtschaft
+ */
+package ch.hsluw.mangelmanager.model;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.GregorianCalendar;
+
+import javax.persistence.*; //ändern
+
+/**
+ * Diese Klasse bildet einen Mangel ab.
+ * 
+ * @version 1.0
+ * @author mmont
+ *
+ */
+
+@Entity
+@NamedQueries({
+		@NamedQuery(name = "Mangel.findByID", query = "SELECT p FROM Mangel p WHERE p.id=:id"),
+		@NamedQuery(name = "Mangel.findByName", query = "SELECT p FROM Mangel p WHERE p.name=:name"),
+		@NamedQuery(name = "Mangel.findByErfassungsZeit", query = "SELECT p FROM Mangel p WHERE p.erfassungsZeit <= erfassungsZeit"),
+		@NamedQuery(name = "Mangel.findByFaelligkeitsDatum", query = "SELECT p FROM Mangel p WHERE p.faelligkeitsDatum=:faelligkeitsDatum"),
+		@NamedQuery(name = "Mangel.findByMangelstatus", query = "SELECT p FROM Mangel p WHERE p.fkMangelstatus=:mangelstatus"),
+		@NamedQuery(name = "Mangel.findByBezeichnung", query = "SELECT p FROM Mangel p WHERE p.bezeichnung=:bezeichnung"),
+		@NamedQuery(name = "Mangel.findByAbschlussZeit", query = "SELECT p FROM Mangel p WHERE p.abschlussZeit <= abschlussZeit"), })
+
+
+abstract class Mangel implements Serializable {
+
+	private static final long serialVersionUID = 6294667886934890151L;
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+	@ManyToOne(cascade = CascadeType.PERSIST)
+	private Integer fkProjekt;
+	private String name;
+	@Temporal(TemporalType.TIMESTAMP)
+	private Timestamp erfassungsZeit;
+	@Temporal(TemporalType.TIMESTAMP)
+	private Timestamp abschlussZeit;
+	@Temporal(TemporalType.DATE)
+	private GregorianCalendar faelligkeitsDatum;
+	@ManyToOne(cascade = CascadeType.PERSIST)
+	private Integer fkMangelstatus;
+	@ManyToOne(cascade = CascadeType.PERSIST)
+	private Integer fkLogin;
+	private String bezeichnung;
+
+	/**
+	 * Constructor
+	 */
+	public Mangel(Integer id, Integer fkProjekt, String name,
+			Timestamp erfassungsZeit, Timestamp abschlussZeit,
+			GregorianCalendar faelligkeitsDatum, Integer fkMangelstatus,
+			Integer fkLogin, String bezeichnung) {
+		super();
+		this.id = id;
+		this.fkProjekt = fkProjekt;
+		this.name = name;
+		this.erfassungsZeit = erfassungsZeit;
+		this.abschlussZeit = abschlussZeit;
+		this.faelligkeitsDatum = faelligkeitsDatum;
+		this.fkMangelstatus = fkMangelstatus;
+		this.fkLogin = fkLogin;
+		this.bezeichnung = bezeichnung;
+	}
+
+	
+	//Getters and Setters
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the fkProjekt
+	 */
+	public Integer getFkProjekt() {
+		return fkProjekt;
+	}
+
+	/**
+	 * @param fkProjekt
+	 *            the fkProjekt to set
+	 */
+	public void setFkProjekt(Integer fkProjekt) {
+		this.fkProjekt = fkProjekt;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the erfassungsZeit
+	 */
+	public Timestamp getErfassungsZeit() {
+		return erfassungsZeit;
+	}
+
+	/**
+	 * @param erfassungsZeit
+	 *            the erfassungsZeit to set
+	 */
+	public void setErfassungsZeit(Timestamp erfassungsZeit) {
+		this.erfassungsZeit = erfassungsZeit;
+	}
+
+	/**
+	 * @return the abschlussZeit
+	 */
+	public Timestamp getAbschlussZeit() {
+		return abschlussZeit;
+	}
+
+	/**
+	 * @param abschlussZeit
+	 *            the abschlussZeit to set
+	 */
+	public void setAbschlussZeit(Timestamp abschlussZeit) {
+		this.abschlussZeit = abschlussZeit;
+	}
+
+	/**
+	 * @return the faelligkeitsDatum
+	 */
+	public GregorianCalendar getFaelligkeitsDatum() {
+		return faelligkeitsDatum;
+	}
+
+	/**
+	 * @param faelligkeitsDatum
+	 *            the faelligkeitsDatum to set
+	 */
+	public void setFaelligkeitsDatum(GregorianCalendar faelligkeitsDatum) {
+		this.faelligkeitsDatum = faelligkeitsDatum;
+	}
+
+	/**
+	 * @return the fkMangelstatus
+	 */
+	public Integer getFkMangelstatus() {
+		return fkMangelstatus;
+	}
+
+	/**
+	 * @param fkMangelstatus
+	 *            the fkMangelstatus to set
+	 */
+	public void setFkMangelstatus(Integer fkMangelstatus) {
+		this.fkMangelstatus = fkMangelstatus;
+	}
+
+	/**
+	 * @return the fkLogin
+	 */
+	public Integer getFkLogin() {
+		return fkLogin;
+	}
+
+	/**
+	 * @param fkLogin
+	 *            the fkLogin to set
+	 */
+	public void setFkLogin(Integer fkLogin) {
+		this.fkLogin = fkLogin;
+	}
+
+	/**
+	 * @return the bezeichnung
+	 */
+	public String getBezeichnung() {
+		return bezeichnung;
+	}
+
+	/**
+	 * @param bezeichnung
+	 *            the bezeichnung to set
+	 */
+	public void setBezeichnung(String bezeichnung) {
+		this.bezeichnung = bezeichnung;
+	}
+
+}
---
+++ 05_Eclipseprojekt/ch.hsluw.mangelmanager.model/src/ch/hsluw/mangelmanager/model/Mangelstatus.java
@@ -0,0 +1,57 @@
+/*
+ * ZWECK: Mangelmanager
+ * MODUL: Softwarekompomenten, HSLU-Wirtschaft
+ */
+package ch.hsluw.mangelmanager.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+/**
+ * Diese Klasse zeigt den Status eines Mangels auf.
+ * 
+ * @version 1.0
+ * @author mmont
+ *
+ */
+
+@Entity
+abstract class Mangelstatus implements Serializable {
+
+	private static final long serialVersionUID = 6294667886934890151L;
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+	private String bezeichnung;
+	
+	
+	public Mangelstatus(Integer id, String bezeichnung) {
+		super();
+		this.id = id;
+		this.bezeichnung = bezeichnung;
+	}
+
+	public Mangelstatus() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public String getBezeichnung() {
+		return bezeichnung;
+	}
+	
+	
+
+	
+
+
+}
