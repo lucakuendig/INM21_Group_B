fe69925e1b2198a862ada8ddc707d526b64ee258
ciho <demircihan93@gmail.com>
2015-04-27 14:30:03 +0200

    new packagename

---
+++ 05_Eclipseprojekt/ch.hsluw.mangelmanager.rmi/src/ch/hsluw/mangelmanager/rmi/meldungstyp/MeldungstypROImpl.java
@@ -0,0 +1,63 @@
+/*
+ * ZWECK: Mangelmanager
+ * MODUL: Softwarekomponenten, HSLU-Wirtschaft
+ */
+
+package ch.hsluw.mangelmanager.rmi.meldungstyp;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.List;
+
+import ch.hsluw.mangelmanager.business.MeldungstypManager;
+import ch.hsluw.mangelmanager.business.MeldungstypManagerImpl;
+import ch.hsluw.mangelmanager.model.Meldung;
+import ch.hsluw.mangelmanager.model.Meldungstyp;
+
+
+
+public class MeldungstypROImpl extends UnicastRemoteObject implements MeldungstypRO {
+
+
+	private static final long serialVersionUID = -8523214358712447146L;
+	
+	private MeldungstypManager meldungstypManager;
+
+	public MeldungstypROImpl() throws RemoteException {
+		meldungstypManager = new MeldungstypManagerImpl();
+	}
+
+	@Override
+	public Meldungstyp add(Meldungstyp entity) throws RemoteException, Exception {
+		return meldungstypManager.add(entity);
+	}
+
+	@Override
+	public Meldungstyp update(Meldungstyp entity) throws RemoteException, Exception {
+		return meldungstypManager.update(entity);
+	}
+
+	@Override
+	public void delete(Meldungstyp entity) throws RemoteException, Exception {
+		meldungstypManager.delete(entity);
+	}
+
+	@Override
+	public void deleteById(Integer id) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Meldung findById(Integer id) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Meldungstyp> findAll() throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
