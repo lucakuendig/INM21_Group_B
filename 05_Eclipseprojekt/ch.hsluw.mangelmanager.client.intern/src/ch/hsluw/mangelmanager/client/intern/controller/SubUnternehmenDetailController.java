6b85be676560665e5b5f2cf596b5132fdd88f1c8
lucakuendig <lkuendig@bluewin.ch>
2015-05-05 09:10:31 +0200

    updated InneresUnternehmen

--- 05_Eclipseprojekt/ch.hsluw.mangelmanager.client.intern/src/ch/hsluw/mangelmanager/client/intern/controller/SubUnternehmenDetailController.java
+++ 05_Eclipseprojekt/ch.hsluw.mangelmanager.client.intern/src/ch/hsluw/mangelmanager/client/intern/controller/SubUnternehmenDetailController.java
@@ -3,9 +3,11 @@ package ch.hsluw.mangelmanager.client.intern.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import ch.hsluw.mangelmanager.client.intern.ClientRMI;
 import ch.hsluw.mangelmanager.model.Subunternehmen;
+import javafx.scene.control.TextField;
 
 public class SubUnternehmenDetailController implements Initializable {
 
@@ -20,7 +22,18 @@ public class SubUnternehmenDetailController implements Initializable {
 		
 		Subunternehmen subunternehmen = null;
 		
-		
+		@FXML
+		public TextField txtSubunternehmenId;
+		@FXML
+		public TextField txtSubunternehmenName;
+		@FXML
+		public TextField txtSubunternehmenTelefon;
+		@FXML
+		public TextField txtSubunternehmenAdresse;
+		@FXML
+		public TextField txtSubunternehmenPlz;
+		@FXML
+		public TextField txtSubunternehmenOrt;
 		
 		@Override
 		public void initialize(URL location, ResourceBundle resources) {
@@ -33,6 +46,12 @@ public class SubUnternehmenDetailController implements Initializable {
 				try {
 				client = ClientRMI.getInstance();
 				subunternehmen = client.getSubunternehmenById(subunternehmenId);
+				txtSubunternehmenId.setText(subunternehmen.getId().toString());
+				txtSubunternehmenName.setText(subunternehmen.getName());
+				txtSubunternehmenTelefon.setText(subunternehmen.getTelefon());
+				txtSubunternehmenAdresse.setText(subunternehmen.getFkAdresse().getStrasse());
+				txtSubunternehmenPlz.setText(subunternehmen.getFkAdresse().getPlz().getPlz().toString());
+				txtSubunternehmenOrt.setText(subunternehmen.getFkAdresse().getPlz().getOrt());
 				
 			} catch (Exception e) {
 				e.printStackTrace();
